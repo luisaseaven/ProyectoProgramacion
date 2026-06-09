82b74418a95326096f6c29f09c4ba0e11898e69f
Oskarcode81 <oskarsegura1981@gmail.com>
2026-06-08 19:48:47 -0500

    Alejandra:Ajustes en clase abstracta Vehiculo y optimización de atributos generales

--- src/co/edu/parqueadero/negocio/Vehiculo.java
+++ src/co/edu/parqueadero/negocio/Vehiculo.java
@@ -1,8 +1,8 @@
 package co.edu.parqueadero.negocio;
 
 public abstract class Vehiculo implements Calculable {
-    protected String placa;
-    protected String tipo;
+    protected String placa; //pedimos el dato (placa)
+    protected String tipo;  //pedimos el dato (tipo)
 
     public Vehiculo(String placa, String tipo) {
         this.placa = placa;
