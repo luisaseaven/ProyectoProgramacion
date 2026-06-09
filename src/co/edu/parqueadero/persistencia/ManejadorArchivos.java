8c67ed5b7df136bd8e6c5646c75984cd6e0e3147
Oskarcode81 <oskarsegura1981@gmail.com>
2026-06-08 19:34:23 -0500

    Alejandra:Persistencia completa e Ajustes en ManejadorArchivos y limpieza de código

--- src/co/edu/parqueadero/persistencia/ManejadorArchivos.java
+++ src/co/edu/parqueadero/persistencia/ManejadorArchivos.java
@@ -1,7 +1,7 @@
 package co.edu.parqueadero.persistencia;
 
 public class ManejadorArchivos {
-    // Aquí la persistencia real 
+    // Aquí la persistencia real es completada 
     public static void guardarDatos(String texto) {
         System.out.println("Datos guardados en el archivo TXT correctamente (Simulado).");
     }
